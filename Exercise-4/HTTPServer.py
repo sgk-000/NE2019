4f892b81babcdc55b72e68d6c6355d8fcff04a00
orz-000 <orzorzorz619@gmail.com>
2019-11-26 18:41:09 +0900

    modify HTTP.py

--- Exercise-4/HTTPServer.py
+++ Exercise-4/HTTPServer.py
@@ -1,6 +1,7 @@
 from socket import *
 import sys
 
+
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverPort = 6789
 serverAddr = '127.0.0.1'
@@ -16,24 +17,31 @@ while True:
     # Set up a new connection from the client
     connectionSocket, clientAddress = serverSocket.accept()
 
-    # Receives the request message from the client
-    message = (connectionSocket.recv(1024)).decode('utf-8')  # should have .decode()
-    # Extract the path of the requested object from the message
-    # The path is the second part of HTTP header, identified by [1]
-    filename = message.split()[1]
-    f = open(filename[1:], encoding='utf-8',errors='ignore')
-    outputdata = f.read() 	# Store the file in a temporary buffer
-    # Send the HTTP response header line to the connection socket
-    connectionSocket.send(("HTTP/1.1 200 OK\r\n\r\n").encode('utf-8'))
+    try:
+        # Receives the request message from the client
+        # should have .decode()
+        message = (connectionSocket.recv(1024)).decode()
+        # Extract the path of the requested object from the message
+        # The path is the second part of HTTP header, identified by [1]
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read() 	# Store the file in a temporary buffer
+        # Send the HTTP response header line to the connection socket
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n")
 
-    # Send the content of the requested file to the connection socket
-    for i in range(0, len(outputdata)):
-        # each time send outputdata[i]
-        connectionSocket.send(outputdata[i].encode('utf-8'))
+        # Send the content of the requested file to the connection socket
+        for i in range(0, len(outputdata)):
+            # each time send outputdata[i]
+            connectionSocket.send(outputdata[i])
 
-    connectionSocket.send(("\r\n").encode('utf-8'))
-    connectionSocket.close()
-
-
-serverSocket.close()
-sys.exit()  # Terminate the program after sending the corresponding data
+        connectionSocket.send("\r\n")
+        connectionSocket.close()
+    except IOError:
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n")
+        connectionSocket.send("<html><body><center><h1>Error 404: File not found</h1></center><p>Head back to <a href=\"/\">dry land</a>.</p></body></html>")
+        print('HTTP/1.1 404 Not Found\r\n')
+        print("Server Address:", serverAddr)
+        print("client Address:", clientAddress)
+        print("Port number:", serverPort)
+        serverSocket.close()
+        sys.exit()  # Terminate the program after sending the corresponding data
